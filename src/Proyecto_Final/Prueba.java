774944e9c1bf1318a0e59faa8f3964af8c485e5a
YaSabesQuienSoy <67292247+YaSabesQuienSoy@users.noreply.github.com>
2020-06-23 16:10:20 -0500

    Create Prueba.java

---
+++ src/Proyecto_Final/Prueba.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyecto_Final;
+
+/**
+ *
+ * @author Ya Sabes Quien Soy =D
+ */
+public class Prueba {
+    
+}
